a5284c74046d30e92ca2f899a603b59a42c07ba0
Tyler Bennet <tylerwbennet@gmail.com>
2024-08-28 07:25:55 -0500

    pushing updated iso image

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-UPLOAD_TOKEN=""
-BINARY_VERSION="6.0.0"
+UPLOAD_TOKEN="glpat-Ss9Vv_cYW5M6ox56xEjk"
+BINARY_VERSION="5.5.0"
 curl -v -H \
   "PRIVATE-TOKEN: $UPLOAD_TOKEN" \
   --progress-bar \
   -o output.txt \
-  --upload-file "SL-Micro-6.0-Packages-x86_64-GM.iso" \
-  "https://gitlab.com/api/v4/projects/61162578/packages/generic/suse_enterprise/$BINARY_VERSION/SL-Micro-6.0-Packages-x86_64-GM.iso"
+  --upload-file "SLE-Micro.x86_64-5.5.0-Default-RT-SelfInstall-GM2.install.iso" \
+  "https://gitlab.com/api/v4/projects/61162578/packages/generic/suse_enterprise/$BINARY_VERSION/SLE-Micro.x86_64-5.5.0-Default-RT-SelfInstall-GM2.install.iso"
