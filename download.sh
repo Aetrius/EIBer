56e474b2b2082debfb4febd3df77758a0bb828c1
Tyler Bennet <tylerwbennet@gmail.com>
2024-08-29 10:36:35 -0500

    updating the package

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl --header "PRIVATE-TOKEN: " \
+curl --header "PRIVATE-TOKEN: glpat-Ss9Vv_cYW5M6ox56xEjk" \
      --retry 5 \
      --retry-connrefused \
-     --output SL-Micro-6.0-Packages-x86_64-GM.iso \
-     "https://gitlab.com/api/v4/projects/61162578/packages/generic/suse_enterprise/6.0.0/SL-Micro-6.0-Packages-x86_64-GM.iso"
+     --output SLE-Micro.x86_64-5.5.0-Default-RT-SelfInstall-GM2.install.iso \
+     "https://gitlab.com/api/v4/projects/61162578/packages/generic/suse_enterprise/5.5.0/SLE-Micro.x86_64-5.5.0-Default-RT-SelfInstall-GM2.install.iso"
