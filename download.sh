3a2ad47d4ab6b32dd16204e2cc9028cec2b06a0d
Tyler Bennet <tylerwbennet@gmail.com>
2024-09-05 00:21:49 -0500

    refactored

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl --header "PRIVATE-TOKEN: glpat-Ss9Vv_cYW5M6ox56xEjk" \
+curl --header "PRIVATE-TOKEN: token" \
      --retry 5 \
      --retry-connrefused \
      --output SLE-Micro.x86_64-5.5.0-Default-RT-SelfInstall-GM2.install.iso \
